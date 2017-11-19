bd42e918ccce7222e8848b1f6876609bfe95f64f
Robert Obreczarek <robert@obreczarek.com>
2017-11-19 12:28:22 +0100

    Added Quiz component, moved assets, updated json

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-    <TheHeader></TheHeader>
+    <the-header></the-header>
+    <quiz-details :quiz="quiz"></quiz-details>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/the-header.vue';
+import QuizDetails from './components/quiz-details.vue';
 
 export default {
   name: 'app',
   components: {
-    TheHeader
+    TheHeader,
+    QuizDetails,
   },
+  data() {
+    return {
+      quiz: null,
+    }
+  },
+  mounted() {
+    fetch('/static/quiz.json')
+      .then((res) => res.json() )
+      .then((quiz) => {
+         this.quiz = quiz;
+      })
+  }
 }
 </script>
---
+++ src/components/quiz-details.vue
@@ -0,0 +1,20 @@
+<template>
+  <section v-if="quiz">
+    <h2>{{ quiz.name }}</h2>
+    <p>{{ quiz.categoryName}}</p>
+    <img :src="quiz.promo" alt="" />
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'QuizDetails',
+    props: {
+      quiz: Object
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
